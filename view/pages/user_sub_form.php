a8ada6445581765f95ed69e2e2183556dd3e0032
TheoT <trolle.theo@gmail.com>
2019-03-25 18:55:17 +0100

    Update UserSub form

--- view/pages/user_sub_form.php
+++ view/pages/user_sub_form.php
@@ -13,7 +13,25 @@
                 </li>
 
                 <li class="amsb-form">
-                    <input class="amsb-item-input" type="text" placeholder='Prenom' name='prenom' required/>
+                    <input class="amsb-item-input" type="text" name='prenom' placeholder='Prenom' required/>
+                </li>
+
+                <li class="amsb-form">
+                    <label for="select_sex">
+                        <select id="select_sex" name="sex">
+                            <option value="">Sex :</option>
+                            <option value="Homme">Homme</option>
+                            <option value="Femme">Femme</option>
+                        </select>
+                    </label>
+                </li>
+
+                <li class="amsb-form">
+                    <input class="amsb-item-input" type="date" name='dateDeNaissance' required/>
+                </li>
+
+                <li class="amsb-form">
+                    <input class="amsb-item-input" type="text" name='icence' placeholder='Licence' required/>
                 </li>
 
                 <li class="amsb-form">
@@ -21,15 +39,25 @@
                 </li>
 
                 <li class="amsb-form">
-                    <input class="amsb-item-input" type="password" name="motDePasse" placeholder="Mot de passe" required>
+                    Le dirigeant ne peux pas mettre le mot de passe du Licencier Techniquement /!\
+                    <input class="amsb-item-input" type="password" name="motDePasse" placeholder="Mot de passe"
+                           required>
                 </li>
 
                 <li class="amsb-form">
-                    <input class="amsb-item-input" type="text" placeholder='Téléphone' name='Telephone' required/>
+                    <input class="amsb-item-input" type="text" name='Telephone' placeholder='Téléphone' required/>
                 </li>
 
                 <li class="amsb-form">
-                    <input class="amsb-item-input" type="text" placeholder='licence' name='Licence' required/>
+                    <input class="amsb-item-input" type="text" name='licence' placeholder='Licence' required/>
+                </li>
+
+                <li class="amsb-form">
+                    Pouvoir importer la photo
+                </li>
+
+                <li class="amsb-form">
+                    Catégorie
                 </li>
 
             </ul>
