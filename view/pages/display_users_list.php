add196ec412248f157730a34adb5e8e0d9e4e6c6
TheoT <trolle.theo@gmail.com>
2019-03-28 14:18:59 +0100

    Update display users list

--- view/pages/display_users_list.php
+++ view/pages/display_users_list.php
@@ -22,23 +22,35 @@
             ?>
         </script>
 
-        <input type="text" onchange="sort_element(this.value, items)">
+
+        <div  class="amsb-display">
+            <input type="text" class="amsb-display-searchBar" onchange="sort_element(this.value, items)" placeholder="Recherche..">
+
+            <ul id="reference_display" class="user_div_reference">
+                <li>Nom</li>
+                <li>Prénom</li>
+                <li>Email</li>
+                <li>Téléphone</li>
+                <li>N° de licence</li>
+                <li>Sexe</li>
+                <li>Catégorie</li>
+            </ul>
         <?php
         foreach ((array) $users_list as $user){
-            echo'<div id="user_'.$user['id'].'" class="user_div">';
-            echo'<p>'.$user['nom'].'</p>';
-            echo'<p>'.$user['prenom'].'</p>';
-            echo'<p>'.$user['mail'].'</p>';
-            echo'<p>'.$user['telephone'].'</p>';
-            echo'<p>'.$user['licence'].'</p>';
-            echo'<p>'.$user['sex'].'</p>';
-            echo'<p>'.$user['categorie'].'</p>';
-            echo'</div>';
+            echo'<ul id="user_'.$user['id'].'" class="user_div">';
+            echo'<li>'.$user['nom'].'</li>';
+            echo'<li>'.$user['prenom'].'</li>';
+            echo'<li>'.$user['mail'].'</li>';
+            echo'<li>'.$user['telephone'].'</li>';
+            echo'<li>'.$user['licence'].'</li>';
+            echo'<li>'.$user['sex'].'</li>';
+            echo'<li>'.$user['categorie'].'</li>';
+            echo'</ul>';
 
             $i++;
         }
         ?>
-        <div id="user_field" class="user_list"></div>
-    </div>
 
+        </div>
+    </div>
 </div>
