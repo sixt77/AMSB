447f3df7e0a285a9c437fb161bc41d28f5dc3714
TheoT <trolle.theo@gmail.com>
2019-03-28 21:28:51 +0100

    Update css create team / match selection

--- view/pages/create_team_form.php
+++ view/pages/create_team_form.php
@@ -1,10 +1,9 @@
 <div id="create_team_form" class="amsb-container-right">
-
     <div class="amsb-container-right-item">
 
         <h2 class="amsb-form-item-title">Création d'équipe !</h2>
 
-        <form class="amsb-form-user_sub_form" action="index.php" method="post">
+        <form action="index.php" method="post">
 
             <div class="amsb-form">
                 <span>Nom :</span>
@@ -21,13 +20,17 @@
                         $i = 1;
                         foreach ((array) $coach_list as $coach){
                             echo'<li class="amsb-display-item">
-                            <span class="amsb-display-item-text">
-                                '.$coach["nom"].' '.$coach["prenom"].'
-                            </span>
-                            <span class="amsb-display-item-radio">
-                                <input class="amsb-display-item-radio-item" type="radio" id="coach_id_'.$i.'" name="coach" value="'.$coach['id_entraineurs'].'">
-                            </span>
-                        </li>';
+                                    <label for="coach_id_'.$i.'">
+                                        <span class="amsb-display-item-text">
+                                            '.$coach["nom"].' '.$coach["prenom"].'
+                                        </span>';
+                                if ($i == 1){
+                                    echo '<input type="radio" id="coach_id_'.$i.'" name="coach" value="'.$coach['id_entraineurs'].'" required>';
+                                } else {
+                                    echo '<input type="radio" id="coach_id_' . $i . '" name="coach" value="' . $coach['id_entraineurs'] . '">';
+                                }
+                            echo'</label>
+                             </li>';
 
                             $i++;
                         }
@@ -40,32 +43,62 @@
 
                     <h2 class="amsb-form-item-title">Choix des Joueurs :</h2>
 
-                    <ul>
+                    <div  class="amsb-display">
+                        <input type="text" id="search_bar" class="amsb-display-searchBar" onchange="sort_element_by_categorie(document.getElementById('categorie_select').value, document.getElementById('surclassage').checked, this.value, items, 'user_div')" placeholder="Recherche..">
+
+                        <select id="categorie_select" name="categorie" onchange="sort_element_by_categorie(this.value, document.getElementById('surclassage').checked, document.getElementById('search_bar').value, items, 'user_div')">
+                            <option value="all">toutes</option>
+                            <option value="U9">U9</option>
+                            <option value="U11">U11</option>
+                            <option value="U13">U13</option>
+                            <option value="U15">U15</option>
+                            <option value="U17">U17</option>
+                            <option value="U18">U18</option>
+                            <option value="U20">U20</option>
+                            <option value="Senior">Senior</option>
+                        </select>
+
+                        <input type="checkbox" id="surclassage" onchange="sort_element_by_categorie(document.getElementById('categorie_select').value, document.getElementById('surclassage').checked, document.getElementById('search_bar').value, items, 'user_div')">
+
+                        <ul id="reference_display" class="user_div_reference">
+                            <li>Nom</li>
+                            <li>Prénom</li>
+                            <li>Email</li>
+                            <li>Téléphone</li>
+                            <li>N° de licence</li>
+                            <li>Sexe</li>
+                            <li>Catégorie</li>
+                        </ul>
+
+                        <div class="amsb-display-scroll">
+
                         <?php
 
                         $i = 1;
                         foreach ((array) $player_list as $player){
-                            echo'<li class="amsb-display-item">
-                                    <label for="player_list_'.$i.'">
-                                        <span class="amsb-display-item-text">
-                                            '.$player["nom"].' '.$player["prenom"].'
-                                        </span>
-                                        
-                                        <input class="amsb-display-item-radio-item" type="checkbox" id="player_list_'.$i.'" name="player_list[]" value="'.$player['id_joueurs'].'">
-                                    </label>
-                                </li>';
+                            echo'<ul>
+                                <label class="user_div" for="player_list_'.$i.'">
+                                    <li>'.$player["nom"].'</li>
+                                    <li>'.$player['prenom'].'</li>
+                                    <li>'.$player['mail'].'</li>
+                                    <li>'.$player['telephone'].'</li>
+                                    <li>'.$player['licence'].'</li>
+                                    <li>'.$player['sex'].'</li>
+                                    <li>'.$player['categorie'].'</li>
+                                    <input type="checkbox" id="player_list_'.$i.'" name="player_list[]" value="'.$player['id_joueurs'].'">
+                                </label>
+                            </ul>';
+
                             $i++;
                         }
 
                         ?>
-                    </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <button type="submit" class="amsb-button" name="create_team" value="">Valider</button>
-
         </form>
-
     </div>
-
 </div>
--- view/pages/match_selection.php
+++ view/pages/match_selection.php
@@ -15,49 +15,38 @@
                 $i = 1;
                 foreach ((array) $match_list as $match){
                     if(isset($match["nb_otm"])){
-                        echo'
-                    <li class="amsb-display-item">
-                        <span class="amsb-display-item-text">
-                            '.$match["date"].' '.$match["lieux"].'
-                        </span>
-                        <span>
-                            '.$match["nb_otm"].'
-                        </span>
-                        <span class="amsb-display-item-radio">
-                            <input class="amsb-display-item-radio-item" type="radio" id="'.$i.'" name="match_id" value="'.$match['id'].'" required>
-                        </span>
-                    </li>
-                ';
+                        echo'<li class="amsb-display-item">
+                                    <label for="'.$i.'">
+                                        <span class="amsb-display-item-text">
+                                            <span>'.date('m/d/Y', $match["date"]).'</span>
+                                            <span>'.$match["lieux"].'</span>
+                                            <span>'.$match["nb_otm"].'</span>
+                                        </span>
+                                        <input type="radio" id="'.$i.'" name="match_id" value="'.$match['id'].'" required>
+                                    </label>
+                               </li>';
                     }
 
                     if(isset($match["nb_arbitres"])){
-                        echo'
-                    <li class="amsb-display-item">
-                        <span class="amsb-display-item-text">
-                            '.$match["date"].' '.$match["lieux"].'
-                        </span>
-                        <span>
-                            '.$match["nb_arbitres"].'
-                        </span>
-                        <span class="amsb-display-item-radio">
-                            <input class="amsb-display-item-radio-item" type="radio" id="'.$i.'" name="match_id" value="'.$match['id'].'" required>
-                        </span>
-                    </li>
-                ';
+                        echo'<li class="amsb-display-item">
+                                    <label for="'.$i.'">
+                                        <span class="amsb-display-item-text">
+                                            <span>'.date('m/d/Y', $match["date"]).'</span>
+                                            <span>'.$match["lieux"].'</span>
+                                            <span>'.$match["nb_arbitres"].'</span>
+                                        </span>
+                                        <input type="radio" id="'.$i.'" name="match_id" value="'.$match['id'].'" required>
+                                    </label>
+                               </li>';
                     }
                     $i++;
                 }
 
                 ?>
 
-                <li class="amsb-form">
-                    <button type="submit" class="amsb-button-confirm" name="<?php echo$get;?>">
-                        Valider
-                    </button>
-                </li>
-
             </ul>
 
+            <button type="submit" class="amsb-button" name="<?php echo$get;?>">Valider</button>
         </form>
 
     </div>
