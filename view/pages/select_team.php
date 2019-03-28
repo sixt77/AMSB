a4e5d81f42b089ca81fe7e47743e96fe784e4ce5
TheoT <trolle.theo@gmail.com>
2019-03-28 18:58:28 +0100

    Update user edit / select team

--- view/pages/select_team.php
+++ view/pages/select_team.php
@@ -6,35 +6,35 @@
 
         <h3 class="amsb-form-item-title-h3">Liste des équipes :</h3>
 
-        <form action="index.php" method="post">
+        <div  class="amsb-display">
+            <input type="text" id="search_bar" class="amsb-display-searchBar" onchange="sort_element_by_categorie(document.getElementById('categorie_select').value, document.getElementById('surclassage').checked, this.value, items, 'user_div')" placeholder="Recherche..">
 
-            <ul>
-                <?php
-                $i = 1;
-                foreach ((array) $team_list as $team){
-                    echo'
-                    <li class="amsb-display-item">
-                        <label for="'.$i.'">
-                            <span class="amsb-display-item-text">'.$team["nom"].'</span>';
+            <form id="form_list_team" action="index.php" method="post">
+                <div class="amsb-display-scroll">
+                        <?php
+                        $i = 1;
+                        foreach ((array) $team_list as $team){
+                            echo'<ul>
+                                    <label for="team_id_'.$i.'" class="user_div">
+                                    
+                                    <li>'.$team['nom'].'</li>';
                             if ($i == 1) {
-                                echo '<input class="amsb-display-item-radio-item" type="radio" id="'.$i.'" name="team" value="'.$team['id_equipes'].'" checked>';
+                                echo '<input type="radio" id="team_id_'.$i.'" name="team" value="'.$team['id_equipes'].'" checked>';
                             } else {
-                                echo '<input class="amsb-display-item-radio-item" type="radio" id="'.$i.'" name="team" value="'.$team['id_equipes'].'">';
+                                echo '<input type="radio" id="team_id_'.$i.'" name="team" value="'.$team['id_equipes'].'">';
                             }
-                            echo '</label>
-                    </li>';
-                    $i++;
-                }
-
-                ?>
+                            echo'</label>
+                                </ul>';
 
 
-            </ul>
+                            $i++;
+                        }
 
-            <button type="submit" class="amsb-button" name="update_team_form" value="">Valider</button>
+                        ?>
+                </div>
 
-        </form>
-
+                <button type="submit" class="amsb-button" name="update_team_form" value="">Valider</button>
+            </form>
+        </div>
     </div>
-
 </div>
--- view/pages/user_edit.php
+++ view/pages/user_edit.php
@@ -2,31 +2,59 @@
     <div class="amsb-container-right-item">
         <h2 class="amsb-form-item-title">Selectionnez un licencier à modifier</h2>
 
-        <form class="amsb-form-user_edit" action="index.php" method="post">
-            <ul>
-                <?php
-                $i = 1;
-                foreach ((array) $user_list as $user){
-                    echo'
-                        <li class="amsb-display-item">
-                            <label for="user_id_'.$i.'">
-                                    <span>'.$user["nom"].'</span>
-                                    <span>'.$user["prenom"].'</span>';
-                                    if ($i == 1) {
-                                        echo '<input type="radio" id="user_id_'.$i.'" name="user_id" value="'.$user['id'].'" checked>';
-                                    } else {
-                                        echo '<input type="radio" id="user_id_'.$i.'" name="user_id" value="'.$user['id'].'">';
-                                    }
-                    echo '</label>
-                        </li>';
-                    $i++;
-                }
-
-                ?>
-
+        <div  class="amsb-display">
+            <input type="text" id="search_bar" class="amsb-display-searchBar" onchange="sort_element_by_categorie(document.getElementById('categorie_select').value, document.getElementById('surclassage').checked, this.value, items, 'user_div')" placeholder="Recherche..">
+            <select id="categorie_select" name="categorie" onchange="sort_element_by_categorie(this.value, document.getElementById('surclassage').checked, document.getElementById('search_bar').value, items, 'user_div')">
+                <option value="all">toutes</option>
+                <option value="U9">U9</option>
+                <option value="U11">U11</option>
+                <option value="U13">U13</option>
+                <option value="U15">U15</option>
+                <option value="U17">U17</option>
+                <option value="U18">U18</option>
+                <option value="U20">U20</option>
+                <option value="Senior">Senior</option>
+            </select>
+            <input type="checkbox" id="surclassage" onchange="sort_element_by_categorie(document.getElementById('categorie_select').value, document.getElementById('surclassage').checked, document.getElementById('search_bar').value, items, 'user_div')">
+            <ul id="reference_display" class="user_div_reference">
+                <li>Nom</li>
+                <li>Prénom</li>
+                <li>Email</li>
+                <li>Téléphone</li>
+                <li>N° de licence</li>
+                <li>Sexe</li>
+                <li>Catégorie</li>
             </ul>
 
-            <button type="submit" class="amsb-button" name="user_edit_form">Valider</button>
-        </form>
+            <form action="index.php" method="post">
+                <div class="amsb-display-scroll">
+                    <?php
+                    $i = 1;
+                    foreach ((array) $user_list as $user){
+                        echo'<ul>
+                                <label class="user_div" for="user_id_'.$i.'">
+                                    <li>'.$user["nom"].'</li>
+                                    <li>'.$user['prenom'].'</li>
+                                    <li>'.$user['mail'].'</li>
+                                    <li>'.$user['telephone'].'</li>
+                                    <li>'.$user['licence'].'</li>
+                                    <li>'.$user['sex'].'</li>
+                                    <li>'.$user['categorie'].'</li>';
+                                        if ($i == 1) {
+                                            echo '<input type="radio" id="user_id_'.$i.'" name="user_id" value="'.$user['id'].'" checked>';
+                                        } else {
+                                            echo '<input type="radio" id="user_id_'.$i.'" name="user_id" value="'.$user['id'].'">';
+                                        }
+                        echo '</label>
+                            </ul>';
+                        $i++;
+                    }
+
+                    ?>
+
+                </div>
+                <button type="submit" class="amsb-button" name="user_edit_form">Valider</button>
+            </form>
+        </div>
     </div>
 </div>
