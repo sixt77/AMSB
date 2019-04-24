a6750a1f7c771bd71ff589e05e7894378b572dbb
TheoT <trolle.theo@gmail.com>
2019-04-24 14:46:00 +0200

    Update css match/arbitre/otm selection

--- view/pages/otm_selection.php
+++ view/pages/otm_selection.php
@@ -2,41 +2,42 @@
 
     <div class="amsb-container-right-item">
 
-        <h2 class="amsb-form-item-title">Séléctionnez des OTM à affecter à ce match</h2>
+        <h2 class="amsb-form-item-title">OTM à affecter à ce match</h2>
 
-        <form action="index.php" method="post">
+        <h3 class="amsb-form-item-title-h3">Liste des licenciés -> OTM :</h3>
 
-            <ul>
+        <div class="amsb-display">
 
-                <?php
-                $i = 0;
-                foreach ((array) $otm_list as $otm){
-                    echo'
-                    <li class="amsb-display-item">
-                        <span class="amsb-display-item-text">
+            <form action="index.php" method="post">
+
+                <ul>
+
+                    <?php
+                    $i = 0;
+                    foreach ((array) $otm_list as $otm){
+                        echo'<li class="amsb-display-item">
+                            <label for="otm_list_id_'.$i.'">';
+                        if(in_array($otm['id'], $otm_sub_list)){
+                            echo'<input class="amsb-display-item-radio-item" type="checkbox" id="otm_list_id_'.$i.'" name="otm_list[]" value="'.$otm['id'].'" checked>';
+                        }else{
+                            echo'<input class="amsb-display-item-radio-item" type="checkbox" id="otm_list_id_'.$i.'" name="otm_list[]" value="'.$otm['id'].'">';
+                        }
+                        echo '<span class="amsb-display-item-text">
                             '.$otm["nom"].' '.$otm["prenom"].'
                         </span>
-
-                        <span class="amsb-display-item-radio">
-                        ';
-                        if(in_array($otm['id'], $otm_sub_list)){
-                            echo'<input class="amsb-display-item-radio-item" type="checkbox" id="'.$i.'" name="otm_list[]" value="'.$otm['id'].'" checked>';
-                        }else{
-                            echo'<input class="amsb-display-item-radio-item" type="checkbox" id="'.$i.'" name="otm_list[]" value="'.$otm['id'].'">';
-                        }
-
-                        echo'</span>
-                    </li>
-                ';
+                        </label>
+                    </li>';
                         $i++;
-                }
+                    }
 
-                ?>
+                    ?>
 
-            </ul>
+                </ul>
 
-            <button type="submit" class="amsb-button" name="<?php echo$get;?>" value="<?php echo$match_id;?>">Valider</button>
-        </form>
+                <button type="submit" class="amsb-button" name="<?php echo$get;?>" value="<?php echo$match_id;?>">Valider</button>
+            </form>
+
+        </div>
 
     </div>
 
