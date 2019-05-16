5036a202f7f32f4ef7a732b9ad583ea04e3cf47c
Rastaaslan <damien.lagorce@gmail.com>
2019-05-16 02:52:29 +0200

    Fonctionnalité : twitter pour la page d'accueil

--- view/pages/main.php
+++ view/pages/main.php
@@ -1,5 +1,8 @@
 <div id="user_edit" class="amsb-container-right">
     <div class="amsb-container-right-item">
         <h2 class="amsb-form-item-title">Accueil de la console</h2>
+        <div class="div_tweet">
+            <a class="twitter-timeline" href="https://twitter.com/AMSB73?ref_src=twsrc%5Etfw">Tweets by AMSB73</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
     </div>
-</div>
+</div>a
